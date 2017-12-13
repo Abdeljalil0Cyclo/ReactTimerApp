fdeea33036a18ed23238b8ae2102056af20c4b43
ABDELJALIL <abdeljalil.aitetaleb@gmail.com>
2017-12-13 17:16:24 +0000

    Initial init

---
+++ app/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main =(props) => {
+  return (
+       <div>
+          <Navigation/>
+          <div className="row">
+            <div className="columns meduim-6 large-4 small-centered">
+              {props.children}
+            </div>
+          </div>
+         
+          
+       </div>
+    );
+}
+
+module.exports=Main;
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var { Link,IndexLink } = require('react-router');
+
+
+var Navigation = React.createClass({
+    render:function() {
+        return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+             <ul className="menu">
+               <li className="menu-text">ReactTimer App</li>
+               <li> <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink> </li>
+               <li> <Link to="/Countdown" activeClassName="active-link" activeStyle={{fontWeight:'bold'}}>Countdown</Link> </li>
+             </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                  <li className="menu-text"> Created By <a href="http://google.com" target="_blank">Abdeljalil</a> </li>
+                </ul>
+            </div>
+        </div>
+          );
+    }
+});
+
+module.exports = Navigation;
